a7531cdba3cdbe417d742b02f549c430b3d94677
Dat <38579777+stevedat@users.noreply.github.com>
2025-02-21 13:13:43 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let pdf;
+
+
+function generatePreview() {
+  // ... (rest of your JavaScript code from before)
+}
+
+function downloadPDF() {
+  // ... (rest of your JavaScript code from before)
+}
